ebfee3aaca1709e65414daf574f1f37dc1b27360
Masayuki Ataka <masayuki.ataka@gmail.com>
2020-05-31 02:23:20 +0900

    Add new file ChatworkApiRequest

--- Sources/MonchCLI/Domain/ValueObject/CreateMessageRequest.swift
+++ Sources/MonchCLI/Domain/ValueObject/CreateMessageRequest.swift
@@ -7,30 +7,6 @@
 
 import Foundation
 
-protocol ApiRequest: Encodable {
-    associatedtype ApiResponse: Decodable
-    var path: String { get }
-    func makeURLRequest(baseUrl: String) -> URLRequest?
-}
-
-protocol ChatworkApiRequest: ApiRequest {}
-
-extension ChatworkApiRequest {
-    func makeURLRequest(baseUrl: String) -> URLRequest? {
-        guard let url = URL(string: "\(baseUrl)/\(path)") else { return nil }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        do {
-            request.httpBody = try XWWWFormUrlEncoder().encode(self)
-        } catch {
-            fatalError(error.localizedDescription)
-        }
-        return request
-    }
-}
-
 struct CreateMessageRequest: ChatworkApiRequest {
     typealias ApiResponse = CreateMessageResponse
 

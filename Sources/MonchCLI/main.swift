dfc24a0c88d616167a22c839c9d064a13d31e5a7
Masayuki Ataka <masayuki.ataka@gmail.com>
2020-05-30 19:21:48 +0900

    add new function getConfig()
    to get data from config.plist

--- Sources/MonchCLI/main.swift
+++ Sources/MonchCLI/main.swift
@@ -1,1 +1,27 @@
-print("Hello, world!")
+import Foundation
+
+func getConfig() throws -> Config {
+    let fileName = "config.plist"
+    #if DEBUG
+    let pathString: String = { (path: String) in
+        URL(fileURLWithPath: path)
+            .pathComponents
+            .dropLast(3)
+            .joined(separator: "/") + "/\(fileName)"
+    }(#file)
+    #else
+    let pathString = FileManager.default.currentDirectoryPath + "/\(fileName)"
+    #endif
+    let pathUrl = URL(fileURLWithPath: pathString)
+
+    let data = try Data(contentsOf: pathUrl)
+    return try PropertyListDecoder().decode(Config.self, from: data)
+}
+
+do {
+    let config = try getConfig()
+    print("Chatwork Token: \(config.chatworkToken)")
+} catch {
+    print(error.localizedDescription)
+}
+

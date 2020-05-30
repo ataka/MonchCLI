f33991c11ae4270015387139785f794ab4bdc64d
Masayuki Ataka <masayuki.ataka@gmail.com>
2020-05-30 17:50:37 +0900

    Rename product name from "MonchCLI" to "monch"

--- Package.swift
+++ Package.swift
@@ -5,13 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "MonchCLI",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+        .executable(name: "monch", targets: ["MonchCLI"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MonchCLI",
             dependencies: []),
@@ -20,3 +17,7 @@ let package = Package(
             dependencies: ["MonchCLI"]),
     ]
 )
+
+// Local Variables:
+// swift-mode:parenthesized-expression-offset: 4
+// End:

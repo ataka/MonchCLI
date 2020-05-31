a1a83f0d66b3215cdabb8cff5565281a8f3f925e
Masayuki Ataka <masayuki.ataka@gmail.com>
2020-06-01 00:00:57 +0900

    Ask deadline and set it to chatwork task

--- Sources/MonchCLI/main.swift
+++ Sources/MonchCLI/main.swift
@@ -52,12 +52,6 @@ func requestCodeReview(for pullRequest: PullRequest, with config: Config, comple
     print("\n> レビュワーを選んでください: ")
     guard let read = readLine() else { return }
 
-    let assignees = read
-        .split(separator: ",")
-        .map(String.init)
-        .compactMap(Int.init)
-        .map { config.reviewers[$0] }
-
     let text = """
     \(pullRequest.title)
     \(pullRequest.url)
@@ -65,8 +59,44 @@ func requestCodeReview(for pullRequest: PullRequest, with config: Config, comple
     レビューをお願いします (please)
     """
 
+    let listDeadline = """
+    [0] 急いでいます。今日中で!!
+    [1] 時間のある時にやって欲しいです。二営業日以内!
+    [2] 急いでいません。でも忘れてもらっては困ります。二週間以内に。
+    """
+    print(listDeadline)
+    print("\n> しめ切りを設定してください: ")
+    guard let readDeadline = readLine() else { return }
+    var dateComponent = DateComponents()
+    switch readDeadline {
+    case "0":
+        dateComponent.hour = 2
+    case "1":
+        switch Calendar.current.dateComponents([.weekday], from: Date()).weekday {
+        case 1, 2, 3, 4: // Sunday, Monday, Tuesday, Wednesday
+            dateComponent.day = 2
+        case 5, 6: // Thursday, Friday
+            dateComponent.day = 4
+        case 7: // Saturday
+            dateComponent.day = 3
+        default:
+            fatalError("NO DAY of WEEK")
+        }
+    case "2":
+        dateComponent.day = 14
+    default:
+        fatalError("No DEADLINE")
+    }
+    guard let deadline = Calendar.current.date(byAdding: dateComponent, to: Date()) else { return }
+
+    let assignees = read
+        .split(separator: ",")
+        .map(String.init)
+        .compactMap(Int.init)
+        .map { config.reviewers[$0] }
+
     //        let request = CreateMessageRequest(roomId: config.chatwork.roomId, text: "Hello, This is MonchCLI!")
-    let request = CreateTaskRequest(roomId: config.chatwork.roomId, text: text, limitType: .none, assigneeIds: assignees.map { $0.chatworkId })
+    let request = CreateTaskRequest(roomId: config.chatwork.roomId, text: text, assigneeIds: assignees.map { $0.chatworkId}, limitType: .date, deadline: deadline)
     let chatworkClient = ChatworkClient(config: config.chatwork)
     chatworkClient.send(request) { taskResponse in
 //        print("TaskIds = \(taskResponse.taskIds)")

e855c1670baaf86bf2dac5b288632cd0a6a8ca59
Masayuki Ataka <masayuki.ataka@gmail.com>
2020-06-07 22:44:16 +0900

    Add schema version for GitHubAuthUserRepository

--- Sources/MonchCLI/Infrastructure/Repository/GitHubAuthUserRepository/GitHubAuthUserRepository.swift
+++ Sources/MonchCLI/Infrastructure/Repository/GitHubAuthUserRepository/GitHubAuthUserRepository.swift
@@ -8,10 +8,15 @@
 import Foundation
 
 struct GitHubAuthUserRepository {
+    private struct Version {
+        static let number: Int = 1
+        static let key = "GitHubAuthUser.Version"
+    }
     private let key = "GitHubAuthUser"
 
     func fetch() -> GitHubUser? {
-        guard let data = UserDefaults.standard.data(forKey: key) else { return nil }
+        guard UserDefaults.standard.integer(forKey: key) == Version.number,
+            let data = UserDefaults.standard.data(forKey: key) else { return nil }
         do {
             return try JSONDecoder().decode(GitHubUser.self, from: data)
         } catch {
@@ -23,6 +28,7 @@ struct GitHubAuthUserRepository {
         do {
             let data = try JSONEncoder().encode(authUser)
             UserDefaults.standard.set(data, forKey: key)
+            UserDefaults.standard.set(Version.number, forKey: Version.key)
         } catch {
             fatalError(error.localizedDescription)
         }
@@ -30,5 +36,6 @@ struct GitHubAuthUserRepository {
 
     func delete() {
         UserDefaults.standard.removeObject(forKey: key)
+        UserDefaults.standard.removeObject(forKey: Version.key)
     }
 }

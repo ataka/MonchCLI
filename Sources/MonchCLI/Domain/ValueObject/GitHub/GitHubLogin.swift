7841c359f40262029744b30ccafb25e622646e11
Masayuki Ataka <masayuki.ataka@gmail.com>
2020-06-13 17:55:44 +0900

    Refactor GitHubLogin, to use default implementation
    thanks goes to
    - 値オブジェクトへのCodable対応 <補足編> #iosdc - Qiita
      - https://qiita.com/maguhiro/items/065de506969fcd68a8de
    - CodableでのValueObject利用 - Qiita
      - https://qiita.com/Mt-Hodaka/items/5e11dcbfb04040fa685f

--- Sources/MonchCLI/Domain/ValueObject/GitHub/GitHubLogin.swift
+++ Sources/MonchCLI/Domain/ValueObject/GitHub/GitHubLogin.swift
@@ -9,25 +9,4 @@ import Foundation
 
 struct GitHubLogin: RawRepresentable, Equatable, Codable {
     let rawValue: String
-
-    init(rawValue: String) {
-        self.rawValue = rawValue
-    }
-
-    // MARK: - Domain Logic
-
-    var isEmpty: Bool { rawValue.isEmpty }
-
-    // MARK: - Codable
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        let value = try container.decode(String.self)
-        self.init(rawValue: value)
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encode(rawValue)
-    }
 }

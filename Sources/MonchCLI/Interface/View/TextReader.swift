e18fde2dd9dd83a76c886ce7cbfe060845e4fafe
Masayuki Ataka <masayuki.ataka@gmail.com>
2020-07-24 06:55:03 +0900

    TextReader asks user if input is empty

--- Sources/MonchCLI/Interface/View/TextReader.swift
+++ Sources/MonchCLI/Interface/View/TextReader.swift
@@ -19,8 +19,43 @@ struct TextReader<T> {
     }
 
     func read() -> Output {
-        print("\n\(message): \n? ", terminator: "")
-        guard let result = readLine() else { fatalError() }
-        return completionHandler(result)
+        func read(with queryText: String) -> Output {
+            print(queryText, terminator: "")
+            let result: String
+            do {
+                result = try readString()
+            } catch let error as ReadStringError {
+                print(error.localizedDescription)
+                return read(with: queryText)
+            } catch {
+                print(error.localizedDescription)
+                fatalError("Unknown error!")
+            }
+            return completionHandler(result)
+        }
+
+        let queryText = "\n\(message): \n? "
+        return read(with: queryText)
+    }
+
+    private func readString() throws -> String {
+        guard let input = readLine() else { throw ReadStringError.readLineFailure }
+        let trimmed = input.trimmingCharacters(in: .whitespaces)
+        guard !trimmed.isEmpty else { throw ReadStringError.inputIsEmpty }
+        return trimmed
+    }
+}
+
+private enum ReadStringError: Error {
+    case readLineFailure
+    case inputIsEmpty
+
+    var localizedDescription: String {
+        switch self {
+        case .readLineFailure:
+            return "読み込みに失敗しました。"
+        case .inputIsEmpty:
+            return "入力が空です。もう一度入力してください。"
+        }
     }
 }

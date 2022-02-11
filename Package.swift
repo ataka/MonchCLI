8ffc425ef2b41c36fec0f011ba709c7bd1fbdcf6
Masayuki Ataka <masayuki.ataka@gmail.com>
2022-02-11 17:04:09 +0900

    Fix issue that Xcode fails to run with Library not loaded error
    Xcode fails to run CommandLine tool with a following error
    ```
    dyld: Library not loaded: @rpath/libswift_Concurrency.dylib
    ```
    
    The following blog post helps how to fix this issue.
    - https://blog.eidinger.info/develop-a-command-line-tool-using-swift-concurrency

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import Foundation
 import PackageDescription
 
 let package = Package(
@@ -23,6 +24,74 @@ let package = Package(
             dependencies: ["MonchCLI"]),
     ]
 )
+
+// https://blog.eidinger.info/develop-a-command-line-tool-using-swift-concurrency
+// IMPORTANT: enable the following function call if you encounter the error
+//    `dyld: Library not loaded: @rpath/libswift_Concurrency.dylib`
+
+hookInternalSwiftConcurrency()
+
+func hookInternalSwiftConcurrency() {
+    let isFromTerminal = ProcessInfo.processInfo.environment.values.contains("/usr/bin/swift")
+    if !isFromTerminal {
+        package.targets.first?.addLinkerSettingUnsafeFlagRunpathSearchPath()
+    }
+}
+
+extension PackageDescription.Target {
+    func addLinkerSettingUnsafeFlagRunpathSearchPath() {
+        linkerSettings = [linkerSetting]
+    }
+
+    private var linkerSetting: LinkerSetting {
+        guard let xcodeFolder = Executable("/usr/bin/xcode-select")("-p") else {
+            fatalError("Could not run `xcode-select -p`")
+        }
+
+        let toolchainFolder = "\(xcodeFolder.trimmed)/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift-5.5/macosx"
+
+        return .unsafeFlags(["-rpath", toolchainFolder])
+    }
+}
+
+extension String {
+    var trimmed: String { trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) }
+}
+
+private struct Executable {
+    private let url: URL
+
+    init(_ filePath: String) {
+        url = URL(fileURLWithPath: filePath)
+    }
+
+    func callAsFunction(_ arguments: String...) -> String? {
+        let process = Process()
+        process.executableURL = url
+        process.arguments = arguments
+
+        let stdout = Pipe()
+        process.standardOutput = stdout
+
+        process.launch()
+        process.waitUntilExit()
+
+        return stdout.readStringToEndOfFile()
+    }
+}
+
+extension Pipe {
+    func readStringToEndOfFile() -> String? {
+        let data: Data
+        if #available(OSX 10.15.4, *) {
+            data = (try? fileHandleForReading.readToEnd()) ?? Data()
+        } else {
+            data = fileHandleForReading.readDataToEndOfFile()
+        }
+
+        return String(data: data, encoding: .utf8)
+    }
+}
 
 // Local Variables:
 // swift-mode:parenthesized-expression-offset: 4
